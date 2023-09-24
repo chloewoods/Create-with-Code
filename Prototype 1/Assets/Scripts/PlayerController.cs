ed372c3971cacf7927d506e41e8037e0f8ea5cb4
chloewoods <cwoods29@qub.ac.uk>
2023-09-24 19:07:36 +0100

    Completed Lesson 1.4
    Added user input to vehicle script to allow for control of vehicle direction

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    // Private Variables
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
     
     // Start is called before the first frame update
 
@@ -16,7 +20,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //Check if there is a left or right input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //Check if there is a up or down input
+        verticalInput = Input.GetAxis("Vertical");
+        
+        //Move the vehicle forward depending on input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        //Also rotate the vehicle right or left depending on input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
