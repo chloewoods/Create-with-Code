2b8cfc1ac7565e6c3158dce8820bb677f876f9eb
chloewoods <cwoods29@qub.ac.uk>
2023-09-24 21:25:17 +0100

    WebGL build

---
+++ Prototype 1/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9 (void);
+extern void FollowPlayer_Update_m6CCD681461C8062489A435B45285AEE84F3B3DB6 (void);
+extern void FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70 (void);
+extern void FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114 (void);
+extern void OtherVehicles_Start_m0033CD5B0F90F6E523FAEB2C3220690691332FFA (void);
+extern void OtherVehicles_Update_mD0B1C7A05A4C853408237E042F3C6F3A8D83FF34 (void);
+extern void OtherVehicles__ctor_mDF37D5A87DFD2A9E48E298652A23D78232FB3E98 (void);
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+extern void PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33 (void);
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[12] = 
+{
+	FollowPlayer_Start_m138697E0740DB2570074B2DB69AE32AF46A994F9,
+	FollowPlayer_Update_m6CCD681461C8062489A435B45285AEE84F3B3DB6,
+	FollowPlayer_LateUpdate_m69B79F77CFC8072526CE0BA2D71179F66AE15B70,
+	FollowPlayer__ctor_mFEF642F6D296A2FBA30A01B637B6BD8DD2446114,
+	OtherVehicles_Start_m0033CD5B0F90F6E523FAEB2C3220690691332FFA,
+	OtherVehicles_Update_mD0B1C7A05A4C853408237E042F3C6F3A8D83FF34,
+	OtherVehicles__ctor_mDF37D5A87DFD2A9E48E298652A23D78232FB3E98,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[12] = 
+{
+	1115,
+	1115,
+	1115,
+	1115,
+	1115,
+	1115,
+	1115,
+	1115,
+	1115,
+	1115,
+	2151,
+	1115,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	12,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
