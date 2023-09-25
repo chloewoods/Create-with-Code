aa990dcb6d9805aa88e2532c366a4f5a20e0da55
chloewoods <cwoods29@qub.ac.uk>
2023-09-25 21:21:15 +0100

    Challenge 1 completed
    Made plane respond to player input, positioned camera beside plane, camera follows plane, propeller spins

--- Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float verticalInput;
+    private float speed = 0.5f;
+    private float rotationSpeed = -100;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +24,6 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
