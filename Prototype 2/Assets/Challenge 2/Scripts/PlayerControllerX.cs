2330cf144115e7930c6dde50284821872c2cde4a
chloewoods <cwoods29@qub.ac.uk>
2023-10-07 17:13:52 +0100

    Challenge 2
    Added Challenge game with fixes.

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float coolDown = 0.5f;
+    private float coolDownTimer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Reduce timer
+        coolDownTimer -= Time.deltaTime;
+
+        // On spacebar press and timer has reached 0, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coolDownTimer <=0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            
+            // Reset timer
+            coolDownTimer = coolDown;
+        }
+    }
+}
