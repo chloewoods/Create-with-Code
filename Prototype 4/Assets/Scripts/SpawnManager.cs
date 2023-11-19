c75b29b958c63fc38d10f98119d040b1b366b856
chloewoods <cwoods29@qub.ac.uk>
2023-11-19 22:40:38 +0000

    Prototype 5
    Create new project and import assets for prototype 5

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    
     public GameObject enemyPrefab;
     public GameObject powerupPrefab;
     private float spawnRange = 8.5f;
